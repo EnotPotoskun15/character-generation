bdb16389cc5d8aea24f56a086bd16a44b0641fc4
EnotPotoskun15 <dragon_all@mail.ru>
2023-07-27 16:52:50 +0300

    Добавлены стили

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var app = new Vue ({
     el: '#app',
     data: {
-        attributes: []
+        attributes: [],
+        infoChar: true
     },
     methods: {
         randomAtribute () {
             this.attributes = [];
+            this.infoChar = false;
 
             for (let i = null; this.attributes.length < 6; this.attributes.push(this.i)) {
             this.i = Math.floor(Math.random()*12 + 6);
