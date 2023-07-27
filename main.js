96eebefbdcc9bedb11840c1d641e63a4fedeaa89
EnotPotoskun15 <dragon_all@mail.ru>
2023-07-27 15:59:28 +0300

    Измененно: генерация каждой характеристики в диапозоне (6-18)

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var app = new Vue ({
     el: '#app',
     data: {
-        attribute: null
+        attributes: []
     },
     methods: {
         randomAtribute () {
-            this.attribute = Math.floor(Math.random()*12 + 6)
+            this.attributes = [];
+
+            for (let i = null; this.attributes.length < 6; this.attributes.push(this.i)) {
+            this.i = Math.floor(Math.random()*12 + 6);
+            }
         }
     }
 })
