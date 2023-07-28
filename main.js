d22055c1a782b90b3866d6458a8b96f8083a54c1
EnotPotoskun15 <dragon_all@mail.ru>
2023-07-28 11:04:38 +0300

    Генерация характеристик персонажа

--- main.js
+++ main.js
@@ -12,8 +12,32 @@ var app = new Vue ({
             for (let i = null; this.attributes.length < 6; this.attributes.push(this.i)) {
             this.i = Math.floor(Math.random()*12 + 6);
             }
+
+            let sum = this.attributes.reduce(function(a, b){
+                return a + b
+                }, 0);
+
+            if (sum < 72) {
+                do {
+                    var min = 18;
+                    for (let i = 0; this.attributes.length > i-1; i++) {
+                        var next = this.attributes[i];
+
+                        if (min > next) {
+                            min = next;
+                        }
+                    }
+
+                    var indexMinChar = this.attributes.indexOf(min);
+                    var newAttribute = Math.floor(Math.random()*12 + 6);
+
+                    this.attributes[indexMinChar] = newAttribute
+
+                    newSum = this.attributes.reduce(function(a, b){
+                        return a + b
+                        }, 0);
+                } while (newSum < 72);
+            }
         }
     }
 })
-
-// Дописать, что бы сумма элементов массива была больше и равна 72. Если сумма меньше 72, то самый минимальный элемент массива перегенерируется.
