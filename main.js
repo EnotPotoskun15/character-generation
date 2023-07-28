385cbb7828f27d824df09258dbb8fc01d2a2616e
EnotPotoskun15 <dragon_all@mail.ru>
2023-07-28 13:40:51 +0300

    Генерация характеристик персонажа

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var app = new Vue ({
             this.attributes = [];
             this.infoChar = false;
 
-            for (let i = null; this.attributes.length < 6; this.attributes.push(this.i)) {
+            for (i = null; this.attributes.length < 6; this.attributes.push(this.i)) {
             this.i = Math.floor(Math.random()*12 + 6);
             }
 
