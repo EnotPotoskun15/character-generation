a35da394702b92581c4f7b05504dbf63ebc84517
EnotPotoskun15 <dragon_all@mail.ru>
2023-07-31 15:19:04 +0300

    update to v 0.02

--- main.js
+++ main.js
@@ -1,42 +1,70 @@
 var app = new Vue ({
     el: '#app',
     data: {
-        attributes: [],
-        infoChar: true
+        generation: false,
+        infoChar: true,
+        nameAttributes: ["СИЛ", "ЛОВ", "ТЕЛ", "ИНТ", "МДР", "ХАР"]
     },
     methods: {
-        randomAtribute () {
-            this.attributes = [];
-            this.infoChar = false;
+        gen() {
+            return this.generation = true;
+        }
+    },
+    computed: {
+        randomAtributes () {
+            if (this.generation) {
 
-            for (i = null; this.attributes.length < 6; this.attributes.push(this.i)) {
-                this.i = Math.floor(Math.random()*12 + 6);
-            }
+                this.attributes = [];
+                this.infoChar = false;
 
-            let sum = this.attributes.reduce(function(a, b){
-                return a + b
-                }, 0);
+                for (i = null; this.attributes.length < 6; this.attributes.push(this.i)) {
+                    this.i = Math.floor(Math.random()*12 + 6);
+                }
 
-            if (sum < 72) {
-                do {
-                    let min = 18;
-                    for (let i = 0; this.attributes.length > i-1; i++) {
-                        next = this.attributes[i];
+                let sum = this.attributes.reduce(function(a, b){
+                    return a + b
+                    }, 0);
 
-                        if (min > next) {
-                            min = next;
+                if (sum < 72) {
+                    do {
+                        let min = 18;
+                        for (let i = 0; this.attributes.length > i-1; i++) {
+                            next = this.attributes[i];
+
+                            if (min > next) {
+                                min = next;
+                            }
                         }
-                    }
 
-                    let indexMinChar = this.attributes.indexOf(min);
-                    let newAttribute = Math.floor(Math.random()*12 + 6);
+                        let indexMinChar = this.attributes.indexOf(min);
+                        let newAttribute = Math.floor(Math.random()*12 + 6);
 
-                    this.attributes[indexMinChar] = newAttribute
+                        this.attributes[indexMinChar] = newAttribute
 
-                    newSum = this.attributes.reduce(function(a, b){
-                        return a + b
-                        }, 0);
-                } while (newSum < 72);
+                        newSum = this.attributes.reduce(function(a, b){
+                            return a + b
+                            }, 0);
+                    } while (newSum < 72);
+                }
+            }
+            return this.attributes;
+        },
+        modifiersAttributes() {
+            if(this.generation && !this.endComputed) {
+
+                this.modifiers = [];
+                this.modifier = 0
+
+                for (i = 0; i < 6; i++) {
+                    this.modifier = Math.floor((this.attributes[i]-10) / 2);
+                    this.modifiers.push(this.modifier);
+                }
+            }
+            return this.modifiers;            
+        },
+        endComputed() {
+            if (this.generation) {
+                this.generation = false;
             }
         }
     }
